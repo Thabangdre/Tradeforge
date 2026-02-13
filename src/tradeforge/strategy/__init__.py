acf1bd98f5147894830a0c6982d40f7e7936358d
Thabangdre <cecilthabang@gmail.com>
2026-02-13 21:12:53 +0200

    Implement EA adapter strategy module and tests

---
+++ src/tradeforge/strategy/__init__.py
@@ -0,0 +1,12 @@
+from .ea_adapter import Action, AccountState, EAAdapter, Order, Strategy, Tick, run_strategy, sma_cross_signal
+
+__all__ = [
+    "Action",
+    "AccountState",
+    "EAAdapter",
+    "Order",
+    "Strategy",
+    "Tick",
+    "run_strategy",
+    "sma_cross_signal",
+]
