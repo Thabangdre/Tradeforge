ed34cf2858637ab306f740f7b8ea0e2a5d60a0d5
Thabangdre <cecilthabang@gmail.com>
2026-02-15 16:39:12 +0200

    Add walk-forward backtesting module with tests

---
+++ tradeforge/__init__.py
@@ -0,0 +1,5 @@
+"""Tradeforge backtesting utilities."""
+
+from .walk_forward import FoldResult, WalkForwardBacktester, WalkForwardConfig
+
+__all__ = ["FoldResult", "WalkForwardBacktester", "WalkForwardConfig"]
